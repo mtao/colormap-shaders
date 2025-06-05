4fb4e16116288ddcc539f1ddbb627b535b177a46
Michael Tao <mtao@dgp.toronto.edu>
2025-06-05 18:07:06 -0400

    updating cpu and removing extra macro

--- tool/transcode.sh
+++ tool/transcode.sh
@@ -3,12 +3,14 @@
 SHADERS="metal:metal
 glsl:frag"
 
+NCPUs=$( lscpu -J | jq '.lscpu[] | select( .field == "CPU(s):" ) | .data' -r )
+#NCPUs=$(sysctl -n hw.ncpu)
 (
 	cd "$(dirname "$0")/../src"
 	for SHADER in $SHADERS; do
 		TYPE=$(echo $SHADER | cut -d: -f1)
 		EXT=$(echo $SHADER | cut -d: -f2)
 		mkdir -p "../shaders/$TYPE"
-		find . -name '*\.txt' | xargs -I{} -L 1 -P $(sysctl -n hw.ncpu) bash -c "../tool/transcode/${TYPE}.sh {} ../shaders/$TYPE/\$(basename {} | cut -d. -f1).${EXT}"
+		find . -name '*\.txt' | xargs -I{} -L 1 -P ${NCPUs} bash -c "../tool/transcode/${TYPE}.sh {} ../shaders/$TYPE/\$(basename {} | cut -d. -f1).${EXT}"
 	done
 )
